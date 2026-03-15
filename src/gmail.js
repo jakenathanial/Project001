091e5d6f21241c3702eb6f8e5baab061e6e25e28
jakenathanial <jakelordclash@gmail.com>
2026-03-15 10:43:23 +0530

    Update gmail.js

--- src/gmail.js
+++ src/gmail.js
@@ -1,1 +1,53 @@
 
+const { google } = require('googleapis');
+
+function getOAuthClient() {
+  const oauth2Client = new google.auth.OAuth2(
+    process.env.GMAIL_CLIENT_ID,
+    process.env.GMAIL_CLIENT_SECRET,
+    process.env.GMAIL_REDIRECT_URI
+  );
+
+  oauth2Client.setCredentials({
+    refresh_token: process.env.GMAIL_REFRESH_TOKEN
+  });
+
+  return oauth2Client;
+}
+
+async function listEmails() {
+  const auth = getOAuthClient();
+  const gmail = google.gmail({ version: 'v1', auth });
+
+  const res = await gmail.users.messages.list({
+    userId: 'me',
+    maxResults: 10
+  });
+
+  return res.data.messages || [];
+}
+
+async function sendEmail(to, subject, body) {
+  const auth = getOAuthClient();
+  const gmail = google.gmail({ version: 'v1', auth });
+
+  const message = [
+    `To: ${to}`,
+    `Subject: ${subject}`,
+    '',
+    body
+  ].join('\n');
+
+  const encoded = Buffer.from(message)
+    .toString('base64')
+    .replace(/\+/g, '-')
+    .replace(/\//g, '_')
+    .replace(/=+$/, '');
+
+  await gmail.users.messages.send({
+    userId: 'me',
+    requestBody: { raw: encoded }
+  });
+}
+
+module.exports = { listEmails, sendEmail };
